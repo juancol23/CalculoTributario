e2dbf06750ee331db197ac6e9eec808bdef227f6
Juan Valdemar Colichon Ramirez <juanvcr@outlook.com>
2017-06-11 06:27:51 -0500

    OFFLINE UNA VERSIOON reemplaza  404

--- app/src/main/java/legalmovil/com/calculotributario/view/offline/Web.java
+++ app/src/main/java/legalmovil/com/calculotributario/view/offline/Web.java
@@ -29,7 +29,7 @@ import legalmovil.com.calculotributario.R;
 import legalmovil.com.calculotributario.view.descargarSitiosOffline.Down;
 
 public class Web extends AppCompatActivity {
-        Button btnSincronizar;
+        Button btnSincronizar, btnRefrest;
         DownloadManager downloadManager;
     WebView webView1;
     @Override
@@ -37,35 +37,56 @@ public class Web extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
 
-        btnSincronizar = (Button) findViewById(R.id.btnSincronizar);
-        webView1 = (WebView) findViewById(R.id.webView1);
+        final File file = new File (Environment
+                .getExternalStorageDirectory()
+                .getPath() + "/Android/data/"
+                + getPackageName()
+                + "/files/sdcard/DirName/index.html");
 
+        btnSincronizar = (Button) findViewById(R.id.btnSincronizar);
+        btnRefrest =  (Button) findViewById(R.id.btnRefrest);
+
+        webView1 = (WebView) findViewById(R.id.webView1);
+        Refrest();
+
+        btnRefrest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (file.exists()) {
+                    webView1.loadUrl("file://"
+                            + Environment
+                            .getExternalStorageDirectory()
+                            .getPath() + "/Android/data/"
+                            + getPackageName()
+                            + "/files/sdcard/DirName/index.html");
+
+                }if(!file.exists()){
+                    webView1.loadUrl("file:///android_asset/www/404.html");
+                }
+            }
+        });
 
         btnSincronizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (Environment.getExternalStorageState().equals(
                         Environment.MEDIA_MOUNTED)) {
-                   /* // execute this when the downloader must be fired
-                    DownloadFile downloadFile = new DownloadFile();
-                    downloadFile.execute("https://legalmovil.com/Codigos/c_de_comercio.html");*/
-                    File file = new File (Environment
-                            .getExternalStorageDirectory()
-                            .getPath() + "/Android/data/"
-                            + getPackageName()
-                            + "/files/sdcard/DirName/index.html");
+
                     if (file.exists()) {
                         file.delete();
                         if (!file.exists()) {
                         downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
                        // Uri uri = Uri.parse("https://legalmovil.com/Codigos/c_de_comercio.html");
-                        Uri uri = Uri.parse("https://legalmovil.com/Codigos/c_de_comercio.html");
+                        Uri uri = Uri.parse("https://legalmovil.com/Codigos/diccionario.html");
                         DownloadManager.Request request = new DownloadManager.Request(uri);
                         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
                         request.setTitle("Título");
-                        request.setDestinationInExternalFilesDir(Web.this,"/sdcard/DirName/","index.html");
+                        request.setDestinationInExternalFilesDir(Web.this,"file:///android_asset/www/","index.html");
                         Long reference = downloadManager.enqueue(request);
                         Toast.makeText(Web.this, "Descarga Completa", Toast.LENGTH_SHORT).show();
+                        Refrest();
+
                     }
                 }
                 } else {
@@ -74,52 +95,23 @@ public class Web extends AppCompatActivity {
             }
         });
     }
-    private class DownloadFile extends AsyncTask<String, Integer, String> {
 
-        @Override
-        protected String doInBackground(String... sUrl) {
-            try {
-
-                URL url = new URL("https://legalmovil.com/Codigos/c_de_comercio.html");
-                URLConnection connection = url.openConnection();
-                connection.connect();
-
-                // download the file
-                InputStream input = new BufferedInputStream(url.openStream());
-                OutputStream output = new FileOutputStream(Environment
-                        .getExternalStorageDirectory().getPath()
-                        + "/data/"
-                        + getPackageName() + "/sdcard/DirName/index.html");
-
-                byte data[] = new byte[1024];
-                int count;
-                try {
-                    while ((count = input.read(data)) != -1)
-                        output.write(data, 0, count);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                output.flush();
-                output.close();
-                input.close();
-
-                Toast.makeText(Web.this, "File downloaded", Toast.LENGTH_SHORT).show();
-
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
-            return null;
-        }
-    }
-
-    public void Refrest(View view){
-        webView1.loadUrl("file://"
-                + Environment
+    public void Refrest(){
+        File files = new File (Environment
                 .getExternalStorageDirectory()
                 .getPath() + "/Android/data/"
                 + getPackageName()
                 + "/files/sdcard/DirName/index.html");
+        if (files.exists()) {
+            webView1.loadUrl("file://"
+                    + Environment
+                    .getExternalStorageDirectory()
+                    .getPath() + "/Android/data/"
+                    + getPackageName()
+                    + "/files/sdcard/DirName/index.html");
+        }if(!files.exists()){
+            webView1.loadUrl("file:///android_asset/www/404.html");
+        }
     }
 }
 
